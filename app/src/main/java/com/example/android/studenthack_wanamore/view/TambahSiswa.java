20e93148795a7316677443a5154133f8c88e07e2
lu William Hanugra <cipatonthesky@gmail.com>
2017-02-20 20:20:18 +0700

    willy_activity

---
+++ app/src/main/java/com/example/android/studenthack_wanamore/view/TambahSiswa.java
@@ -0,0 +1,16 @@
+package com.example.android.studenthack_wanamore.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.studenthack_wanamore.R;
+
+public class TambahSiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_siswa);
+
+    }
+}
