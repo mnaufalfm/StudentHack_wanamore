fbbed45ea8bc61376874e6d44eb5017d7fe40282
lu William Hanugra <cipatonthesky@gmail.com>
2017-02-24 18:05:21 +0700

    tambah siswa

--- app/src/main/java/com/example/android/studenthack_wanamore/view/TambahSiswa.java
+++ app/src/main/java/com/example/android/studenthack_wanamore/view/TambahSiswa.java
@@ -2,15 +2,25 @@ package com.example.android.studenthack_wanamore.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.android.studenthack_wanamore.R;
 
 public class TambahSiswa extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_siswa);
+
+        toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Tambah Siswa");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 }
