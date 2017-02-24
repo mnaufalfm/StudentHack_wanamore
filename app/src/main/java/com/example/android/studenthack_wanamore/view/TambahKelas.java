5ee77d1925731fde745c61df3c8472a568696c70
lu William Hanugra <cipatonthesky@gmail.com>
2017-02-24 18:21:47 +0700

    Tambah Kelas

---
+++ app/src/main/java/com/example/android/studenthack_wanamore/view/TambahKelas.java
@@ -0,0 +1,23 @@
+package com.example.android.studenthack_wanamore.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class TambahKelas extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_kelas);
+
+        toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Tambah Kelas");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+}
